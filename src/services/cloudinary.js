776a3bf48b0a3e808c6228141afe9d563e249076
BarretoDev <barretoads.dev@gmail.com>
2024-10-06 13:34:21 -0600

    Deploy to GitHub

--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -1,11 +1,21 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 // Configura Cloudinary con tus credenciales
+envVarsCheck();
 cloudinary.config({
-  cloud_name: import.meta.env.CLOUDINARY_CLOUD_NAME,
-  key_name: import.meta.env.CLOUDINARY_KEY_NAME,
-  api_key: import.meta.env.CLOUDINARY_API_KEY,
-  api_secret: import.meta.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+// Verifica si las variables de entorno están definidas
+function envVarsCheck() {
+  const requiredVars = ['CLOUDINARY_CLOUD_NAME', 'CLOUDINARY_API_KEY', 'CLOUDINARY_API_SECRET'];
+  requiredVars.forEach((variable) => {
+    if (!process.env[variable]) {
+      console.warn(`La variable de entorno ${variable} no está definida. Por favor, configúrala adecuadamente.`);
+    }
+  });
+}
 
 export default cloudinary;
