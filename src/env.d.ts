84bd8f4e188b329cde80009612987972a2f08f3b
BarretoDev <barretoads.dev@gmail.com>
2024-10-07 14:17:22 -0600

    Deploy to GitHub

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
-type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+import type { Runtime as CloudflareRuntime } from "@astrojs/cloudflare";
+
+interface Env {
+  // Define your environment-specific variables here, e.g.
+  // MY_API_KEY: string;
+}
 
 declare namespace App {
-	interface Locals extends Runtime {}
+  interface Locals extends CloudflareRuntime<Env> {}
 }
--- src/services/cloudinary.js
+++ src/services/cloudinary.js
@@ -3,14 +3,14 @@ import { v2 as cloudinary } from 'cloudinary';
 // Configura Cloudinary con tus credenciales
 envVarsCheck();
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_KEY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Verifica si las variables de entorno están definidas
 function envVarsCheck() {
-  const requiredVars = ['CLOUDINARY_CLOUD_NAME', 'CLOUDINARY_API_KEY', 'CLOUDINARY_API_SECRET'];
+  const requiredVars = ['CCLOUDINARY_KEY_NAME', 'CLOUDINARY_API_KEY', 'CLOUDINARY_API_SECRET'];
   requiredVars.forEach((variable) => {
     if (!process.env[variable]) {
       console.warn(`La variable de entorno ${variable} no está definida. Por favor, configúrala adecuadamente.`);
